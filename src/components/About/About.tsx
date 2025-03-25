20f258dd76bbe37e1472bf1b8374c34cdd01a6ca
kmckoy14 <keatonsport@gmail.com>
2025-03-25 16:32:49 -0700

    Mistake: Put the hero section in about me. Made the correction.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,15 @@
-
+import styles from "./About.module.css"
 
 const About = () => {
   return (
-    <section>
+    <section className={styles.hero}>
+      <div className="container">
+        <h1>Software Engineer & Solutions Architech</h1>
+        <p>I'm a problem solver with a passion for creating innovative solutions.</p>
+        <p>Currently seeking software engineering internship opportunities.</p>
+        <a href="" className="btn">View My Work</a>
+        <a href="" className="btn btn-outline">Contact Me</a>
+      </div>
       
     </section>
   )
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,17 @@
-
+import styles from "./Hero.module.css"
 
 const Hero = () => {
   return (
-    <div>
+    <section className={styles.hero}>
+      <div className="container">
+        <h1>Software Engineer & Solutions Architech</h1>
+        <p>I'm a problem solver with a passion for creating innovative solutions.</p>
+        <p>Currently seeking software engineering internship opportunities.</p>
+        <a href="" className="btn">View My Work</a>
+        <a href="" className="btn btn-outline">Contact Me</a>
+      </div>
       
-    </div>
+    </section>
   )
 }
 
