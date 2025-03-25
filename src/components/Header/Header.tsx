a0a6b67fcc88af88043a2aa9f19756cf7a76ad70
kmckoy14 <keatonsport@gmail.com>
2025-03-25 16:19:23 -0700

    Styled header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,14 @@ const Header = () => {
     <header className={styles.header}>
           <div className="container">
               <nav>
-                  <div className={styles.logo}>Keaton McKoy</div>
+                  <div className={styles.logo}>
+                      <a href="">Keaton McKoy</a>
+                  </div>
                   <div className={styles.menu}>
                       <a className={styles.menu_items} href="#about">About</a>
                       <a className={styles.menu_items} href="#projects">Projects</a>
                       <a className={styles.menu_items} href="#contact">Contact</a>
-                      <a className={styles.menu_items} href="https://github.com/kmckoy14">Github</a>
+                      <a className={styles.menu_items} href="https://github.com/kmckoy14">GitHub</a>
                   </div>
               </nav>
         </div>
